db90e5be8f742455504fca5d74a2a5a0b94844e9
SergeiGrub <sergeygrubber@gmail.com>
2024-11-05 15:55:39 +0300

    Initial commit

---
+++ src/test/java/ru/fnpr/fnpr_telegram_bot/FnprTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fnpr.fnpr_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnprTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
