4af0c9b1f11aceec5b33e3f43d9a7653789977c1
SergeiGrub <sergeygrubber@gmail.com>
2024-11-05 17:10:37 +0300

    Создание классов

---
+++ src/main/java/ru/fnpr/fnpr_telegram_bot/controller/BotController.java
@@ -0,0 +1,8 @@
+package ru.fnpr.fnpr_telegram_bot.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BotController {
+    // Здесь будет логика обработки сообщений и команд
+}
